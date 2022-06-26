d574eb256e59f5271e73d2d0cf9275345099032e
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2022-06-26 16:09:24 +0530

    Update event.ts

--- src/event.ts
+++ src/event.ts
@@ -2,14 +2,14 @@ import type http from 'http'
 import type { IncomingMessage, ServerResponse, Handler, Middleware } from './types'
 import { callHandler } from './handler'
 
-export interface H3EventContext extends Record<string, any> {}
+import { createApp } from "h3"
 
 export interface H3Event {
   '__is_event__': true
   event: H3Event
   req: IncomingMessage
   res: ServerResponse
-  context: H3EventContext
+  context: Record<string, any>
 }
 
 export type CompatibilityEvent = H3Event | IncomingMessage
@@ -25,6 +25,12 @@ export function defineEventHandler <T = any> (handler: EventHandler<T>): EventHa
   handler.__is_handler__ = true
   return handler
 }
+export function defineEventHandlers < T=any > (...handlers: Array<EventHandler<T>>): EventHandler<T>{
+  const app = createApp()
+  app.use(handlers.map((func) => defineEventHandler(func)))
+  return app
+}
+
 export const eventHandler = defineEventHandler
 
 export type LazyEventHandler = () => EventHandler | Promise<EventHandler>
