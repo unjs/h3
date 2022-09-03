a9b992efad9280d68f02807ef9094b46bb7adb39
Pooya Parsa <pooya@pi0.io>
2022-09-03 18:02:35 +0200

    fix(sendRedirect): only encode required chars in meta tag

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -24,15 +24,10 @@ export function defaultContentType (event: CompatibilityEvent, type?: string) {
 }
 
 export function sendRedirect (event: CompatibilityEvent, location: string, code = 302) {
-  const encodedLoc = encodeURI(decodeURI(location))
   event.res.statusCode = code
-  event.res.setHeader('Location', encodedLoc)
-  // Minimal html document that redirects on client side
-  const html = `<!DOCTYPE html>
-<html>
-  <head><meta http-equiv="refresh" content="0; url=${encodedLoc}"></head>
-  <body>Redirecting to <a href=${JSON.stringify(encodedLoc)}>${encodedLoc}</a></body>
-</html>`
+  event.res.setHeader('Location', location)
+  const encodedLoc = location.replace(/"/g, '%22')
+  const html = `<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=${encodedLoc}"></head></html>`
   return send(event, html, MIMES.html)
 }
 
