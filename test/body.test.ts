240030549cb951c067b428577361cfbc476775da
Tobias Diez <code@tobiasdiez.com>
2022-10-16 21:36:30 +0000

    feat: useBody takes content-type header into account

--- test/body.test.ts
+++ test/body.test.ts
@@ -1,6 +1,6 @@
 import supertest, { SuperTest, Test } from 'supertest'
 import { describe, it, expect, beforeEach } from 'vitest'
-import { createApp, toNodeListener, App, useBody, useRawBody, eventHandler, readBody } from '../src'
+import { createApp, toNodeListener, App, readBody, readRawBody, eventHandler } from '../src'
 
 describe('', () => {
   let app: App
@@ -11,10 +11,10 @@ describe('', () => {
     request = supertest(toNodeListener(app))
   })
 
-  describe('useRawBody', () => {
+  describe('readRawBody', () => {
     it('can handle raw string', async () => {
       app.use('/', eventHandler(async (request) => {
-        const body = await useRawBody(request)
+        const body = await readRawBody(request)
         expect(body).toEqual('{"bool":true,"name":"string","number":1}')
         return '200'
       }))
@@ -24,6 +24,30 @@ describe('', () => {
         number: 1
       }))
 
+      expect(result.text).toBe('200')
+    })
+
+    it('returns empty string if body is not present', async () => {
+      let body
+      app.use('/', eventHandler(async (request) => {
+        body = await readRawBody(request)
+        return '200'
+      }))
+      const result = await request.post('/api/test')
+
+      expect(body).toBe('')
+      expect(result.text).toBe('200')
+    })
+
+    it('returns an empty string if body is empty', async () => {
+      let body
+      app.use('/', eventHandler(async (request) => {
+        body = await readRawBody(request)
+        return '200'
+      }))
+      const result = await request.post('/api/test').send('')
+
+      expect(body).toBe('')
       expect(result.text).toBe('200')
     })
   })
@@ -50,7 +74,7 @@ describe('', () => {
 
     it('parse the form encoded into an object', async () => {
       app.use('/', eventHandler(async (request) => {
-        const body = await useBody(request)
+        const body = await readBody(request)
         expect(body).toMatchObject({
           field: 'value',
           another: 'true',
@@ -61,6 +85,42 @@ describe('', () => {
       const result = await request.post('/api/test')
         .send('field=value&another=true&number=20')
 
+      expect(result.text).toBe('200')
+    })
+
+    it('returns empty string if body is not present with text/plain', async () => {
+      let body
+      app.use('/', eventHandler(async (request) => {
+        body = await readBody(request)
+        return '200'
+      }))
+      const result = await request.post('/api/test').set('Content-Type', 'text/plain')
+
+      expect(body).toBe('')
+      expect(result.text).toBe('200')
+    })
+
+    it('returns empty object if body is not present with json', async () => {
+      let body
+      app.use('/', eventHandler(async (request) => {
+        body = await readBody(request)
+        return '200'
+      }))
+      const result = await request.post('/api/test').set('Content-Type', 'application/json')
+
+      expect(body).toBe({})
+      expect(result.text).toBe('200')
+    })
+
+    it('returns the string if content type is plain/text', async () => {
+      let body
+      app.use('/', eventHandler(async (request) => {
+        body = await readBody(request)
+        return '200'
+      }))
+      const result = await request.post('/api/test').set('Content-Type', 'text/plain').send('{ "hello": true }')
+
+      expect(body).toBe('{ "hello": true }')
       expect(result.text).toBe('200')
     })
   })
