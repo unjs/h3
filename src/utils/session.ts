543d136027c62c4bc14db1b7a4030cd5bb38120f
gaokefei <260480378@qq.com>
2023-02-14 17:39:25 +0800

    refactor(clearSession): set the config parameter optional

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -202,7 +202,7 @@ export async function unsealSession(
   return unsealed;
 }
 
-export async function clearSession(event: H3Event, config: SessionConfig) {
+export async function clearSession(event: H3Event, config: Partial<SessionConfig> = {}) {
   const sessionName = config.name || DEFAULT_NAME;
   if (event.context.sessions?.[sessionName]) {
     delete event.context.sessions![sessionName];
