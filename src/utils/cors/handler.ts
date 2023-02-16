855b59da1f9dfbcb0df2bda1d5d2bbb1d9d48a40
Pooya Parsa <pooya@pi0.io>
2023-02-16 15:02:24 +0100

    avoid destructure

--- src/utils/cors/handler.ts
+++ src/utils/cors/handler.ts
@@ -9,13 +9,10 @@ import {
 import type { H3CorsOptions } from "./types";
 
 export function handleCors(event: H3Event, options: H3CorsOptions) {
-  const {
-    preflight: { statusCode },
-  } = resolveCorsOptions(options);
-
+  const _options = resolveCorsOptions(options);
   if (isPreflightRequest(event)) {
     appendCorsPreflightHeaders(event, options);
-    sendNoContent(event, statusCode);
+    sendNoContent(event, _options.preflight.statusCode);
   } else {
     appendCorsActualRequestHeaders(event, options);
   }
