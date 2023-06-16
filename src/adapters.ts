3ac5cf51f757f85932b4c33c580e74d2e40cb639
Hebilicious <xsh4k3@gmail.com>
2023-06-16 15:30:43 +0700

    feat: add adapterFetch

--- src/adapters.ts
+++ src/adapters.ts
@@ -1,17 +1,39 @@
 import { App, createEvent } from "./";
 
+/**
+ * Adapter that helps return a Response from a Request.
+ * It can be used with something like Nitro.
+ * const localResponse = adapterFetch(h3app)
+ * const response = localResponse(request: Request, {context: { cf: request.cf, cloudflare: { request, env, context } }})
+ * @param app
+ * @returns Promise<Response>
+ */
+export const adapterFetch = (app: App) => {
+  return async (request: Request, context?: Record<string, any>) => {
+    try {
+      const event = createEvent(undefined, undefined, request);
+      if (context) {
+        event.context = context;
+      }
+      return (await app.handler(event)) as Response;
+    } catch (error: any) {
+      return new Response(error.toString(), {
+        status: Number.parseInt(error.statusCode || error.code) || 500,
+        statusText: error.statusText,
+      });
+    }
+  };
+};
+
 export const adapterCloudflareWorker = (app: App) => {
   const worker = {
     async fetch(request: Request, env?: any, context?: any) {
-      try {
-        const event = createEvent(undefined, undefined, request);
-        event.context = { cloudflare: { env, context } };
-        const response = (await app.handler(event)) as Response;
-        return response;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
+      const fetchResponse = adapterFetch(app);
+      return await fetchResponse(request, {
+        // @ts-expect-error
+        cf: request.cf,
+        cloudflare: { env, context },
+      });
     },
     fire: () => {
       console.log("implement service worker syntax ...");
