a3f31895c52de8f6213df54a6560bcfeacd8d562
Javad Mnjd <javad.mnjd@hotmail.com>
2023-07-05 13:57:16 +0330

    fix(proxy): make response.body check more general

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -133,7 +133,7 @@ export async function sendProxy(
     return event.node.res.end(data);
   }
 
-  if (response.body !== null) {
+  if (response.body) {
     // Send as stream
     for await (const chunk of response.body as any as AsyncIterable<Uint8Array>) {
       event.node.res.write(chunk);
