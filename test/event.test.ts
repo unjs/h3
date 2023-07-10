beccd83621c6fd879aadf87a15ea91d05989c0fc
Pooya Parsa <pooya@pi0.io>
2023-07-10 14:33:36 +0200

    update

--- test/event.test.ts
+++ test/event.test.ts
@@ -8,7 +8,9 @@ import {
   getMethod,
 } from "../src";
 
-describe("", () => {
+const supportsHeaders = typeof Headers !== "undefined";
+
+describe("Event", () => {
   let app: App;
   let request: SuperTest<Test>;
 
@@ -17,32 +19,30 @@ describe("", () => {
     request = supertest(toNodeListener(app));
   });
 
-  describe("Event", () => {
-    it("can read the method", async () => {
-      app.use(
-        "/",
-        eventHandler((event) => {
-          expect(event.method).toBe(getMethod(event));
-          expect(event.method).toBe("POST");
-          return "200";
-        })
-      );
-      const result = await request.post("/hello");
-      expect(result.text).toBe("200");
-    });
+  it("can read the method", async () => {
+    app.use(
+      "/",
+      eventHandler((event) => {
+        expect(event.method).toBe(getMethod(event));
+        expect(event.method).toBe("POST");
+        return "200";
+      })
+    );
+    const result = await request.post("/hello");
+    expect(result.text).toBe("200");
+  });
 
-    it.runIf(typeof Headers !== undefined)("can read the headers", async () => {
-      app.use(
-        "/",
-        eventHandler((event) => {
-          return {
-            headers: [...event.headers.entries()],
-          };
-        })
-      );
-      const result = await request.post("/hello").set("X-A", "works");
-      const { headers } = JSON.parse(result.text);
-      expect(headers.find(([key]) => key === "x-a")[1]).toBe("works");
-    });
+  it.runIf(supportsHeaders)("can read the headers", async () => {
+    app.use(
+      "/",
+      eventHandler((event) => {
+        return {
+          headers: [...event.headers.entries()],
+        };
+      })
+    );
+    const result = await request.post("/hello").set("X-A", "works");
+    const { headers } = JSON.parse(result.text);
+    expect(headers.find(([key]) => key === "x-a")[1]).toBe("works");
   });
 });
