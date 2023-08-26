840163559ffcb7015987751ed6c6f5e25848a056
Pooya Parsa <pooya@pi0.io>
2023-08-26 21:47:33 +0200

    refactor: use native `Headers` and `Response` for legacy polyfills

---
+++ src/event/_polyfills.ts
@@ -0,0 +1,11 @@
+/**
+ * @deprecated Please use native web Headers
+ * https://developer.mozilla.org/en-US/docs/Web/API/Headers
+ */
+export const H3Headers = globalThis.Headers;
+
+/**
+ * @deprecated Please use native web Headers
+ * https://developer.mozilla.org/en-US/docs/Web/API/Headers
+ */
+export const H3Response = globalThis.Response;
