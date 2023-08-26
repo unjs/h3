1fb8108294853132cfa5214a8a70142c25ab1eed
Pooya Parsa <pooya@pi0.io>
2023-08-26 21:51:02 +0200

    update comment

--- src/event/_polyfills.ts
+++ src/event/_polyfills.ts
@@ -5,7 +5,7 @@
 export const H3Headers = globalThis.Headers;
 
 /**
- * @deprecated Please use native web Headers
- * https://developer.mozilla.org/en-US/docs/Web/API/Headers
+ * @deprecated Please use native web Response
+ * https://developer.mozilla.org/en-US/docs/Web/API/Response
  */
 export const H3Response = globalThis.Response;
