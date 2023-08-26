27082e435b7c7f262607f381873866600db2a5d3
Pooya Parsa <pooya@pi0.io>
2023-08-26 13:03:52 +0200

    move comment outside

--- src/utils/internal/object.ts
+++ src/utils/internal/object.ts
@@ -1,5 +1,5 @@
+// TODO: Benchmark typeof vs try/catch (i guess try/catch is faster in non-edge cases!)
 export function hasProp(obj: object, prop: string | symbol) {
-  // TODO: Benchmark typeof vs try/catch (i guess try/catch is faster in non-edge cases!)
   try {
     return prop in obj;
   } catch {
