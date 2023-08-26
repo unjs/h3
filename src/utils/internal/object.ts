77c4231832f13724f84a3591191a6d4255d00af9
Pooya Parsa <pooya@pi0.io>
2023-08-26 13:03:17 +0200

    ocd: fix comment

--- src/utils/internal/object.ts
+++ src/utils/internal/object.ts
@@ -1,5 +1,5 @@
 export function hasProp(obj: object, prop: string | symbol) {
-  // TODO: Benchmark typeof vs try/catch (i guess try catch is faster in non-edge cases!)
+  // TODO: Benchmark typeof vs try/catch (i guess try/catch is faster in non-edge cases!)
   try {
     return prop in obj;
   } catch {
