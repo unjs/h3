1cf226d2cebc829082ccc7cb90619c5e00764c36
Pooya Parsa <pooya@pi0.io>
2023-08-26 13:02:29 +0200

    update check

--- src/utils/internal/object.ts
+++ src/utils/internal/object.ts
@@ -1,3 +1,8 @@
-export function hasProp(obj: any, prop: string | symbol) {
-  return Object.prototype.hasOwnProperty.call(obj, prop);
+export function hasProp(obj: object, prop: string | symbol) {
+  // TODO: Benchmark typeof vs try/catch (i guess try catch is faster in non-edge cases!)
+  try {
+    return prop in obj;
+  } catch {
+    return false;
+  }
 }
