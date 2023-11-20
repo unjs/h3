c67121d596c9cf2d294b21516ca7e785a97e7b98
Alexander Lichter <github@lichter.io>
2023-11-20 03:26:13 +0100

    docs: improve jsdocs (#574)

--- src/utils/internal/validate.ts
+++ src/utils/internal/validate.ts
@@ -9,6 +9,14 @@ export type ValidateFunction<T> = (
   data: unknown,
 ) => ValidateResult<T> | Promise<ValidateResult<T>>;
 
+/**
+ * Validates the given data using the provided validation function.
+ * @template T The expected type of the validated data.
+ * @param data The data to validate.
+ * @param fn The validation function to use - can be async.
+ * @returns A Promise that resolves with the validated data if it passes validation, meaning the validation function does not throw and returns a value other than false.
+ * @throws {ValidationError} If the validation function returns false or throws an error.
+ */
 export async function validateData<T>(
   data: unknown,
   fn: ValidateFunction<T>,
