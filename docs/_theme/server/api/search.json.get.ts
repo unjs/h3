8373e229bb37170745b8d9fa6e752999f3d27ab5
Estéban <esteban.soubiran@insa-cvl.fr>
2024-01-25 22:14:34 +0100

    feat: move to unjs/docs (#629)

--- docs/_theme/server/api/search.json.get.ts
+++
@@ -1,7 +0,0 @@
-import { serverQueryContent } from "#content/server";
-
-export default eventHandler(async (event) => {
-  return await serverQueryContent(event)
-    .where({ _type: "markdown", navigation: { $ne: false } })
-    .find();
-});
