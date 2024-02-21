e9974e72a3fbac89087d5ba3ecf084d1c7a921f5
bramkamies <bramkamies@passionatepeople.io>
2024-02-21 09:57:13 +0100

    fix: test Buffer and Uint8Array for pass-through serialization

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -53,9 +53,11 @@ describe("", () => {
       { value: { field: 1 }, output: '{"field":1}' },
       { value: [1, 2, 3], output: '[1,2,3]' },
       { value: () => {}, output: undefined },
+      { value: Buffer.from("Hello, world!"), output: Buffer.from("Hello, world!") },
+      { value: Uint8Array.from([1,2,3]), output: Uint8Array.from([1,2,3]) }
     ])('$value => $output', ({ value, output }) => {
       const serialized = serializeIterableValue(value);
-      expect(serialized).toBe(output);
+      expect(serialized).toStrictEqual(output);
     });
   });
 
