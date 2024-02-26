8f0ad346f4fc682b51b80295ef7ef5978ec20755
Michael Brevard <yonshi29@gmail.com>
2024-02-26 18:27:21 +0200

    refactor: cast to Status

--- src/error.ts
+++ src/error.ts
@@ -6,6 +6,7 @@ import {
   sanitizeStatusCode,
 } from "./utils";
 import { hasProp } from "./utils/internal/object";
+import type { Status } from "./types";
 
 /**
  * H3 Runtime Error
@@ -167,7 +168,7 @@ export function sendError(
   }
   const _code = Number.parseInt(
     h3Error.statusCode as unknown as string,
-  ) as const;
+  ) as Status;
   setResponseStatus(event, _code, h3Error.statusMessage);
   event.node.res.setHeader("content-type", MIMES.json);
   event.node.res.end(JSON.stringify(responseBody, undefined, 2));
