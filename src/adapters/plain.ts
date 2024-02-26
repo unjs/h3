a8c16c8e384f7a2a69854c6baf77d6f38a62de60
Michael Brevard <yonshi29@gmail.com>
2024-02-26 19:00:16 +0200

    fix: provide status

--- src/adapters/plain.ts
+++ src/adapters/plain.ts
@@ -2,7 +2,7 @@ import { IncomingMessage } from "node:http";
 import { IncomingMessage as NodeIncomingMessage } from "unenv/runtime/node/http/_request";
 import { ServerResponse as NodeServerResponse } from "unenv/runtime/node/http/_response";
 import type { App } from "../app";
-import type { HTTPMethod } from "../types";
+import type { HTTPMethod, Status } from "../types";
 import { createError, isError, sendError } from "../error";
 import { H3Event, createEvent, eventHandler } from "../event";
 import {
