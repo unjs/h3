d30d7cd9950a73d55237a0014bc078c1eda68f04
Michael Brevard <yonshi29@gmail.com>
2024-02-26 18:28:32 +0200

    fix: use as unknown

--- src/error.ts
+++ src/error.ts
@@ -168,7 +168,7 @@ export function sendError(
   }
   const _code = Number.parseInt(
     h3Error.statusCode as unknown as string,
-  ) as Status;
+  ) as unknown as Status;
   setResponseStatus(event, _code, h3Error.statusMessage);
   event.node.res.setHeader("content-type", MIMES.json);
   event.node.res.end(JSON.stringify(responseBody, undefined, 2));
