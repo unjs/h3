14106ad2448a08501bbfce9e256682d2f3a76a38
Pooya Parsa <pooya@pi0.io>
2024-06-19 14:18:05 +0200

    fix(plain): import from `unenv/runtime/node/http`

--- src/adapters/plain.ts
+++ src/adapters/plain.ts
@@ -1,6 +1,8 @@
 import { IncomingMessage } from "node:http";
-import { IncomingMessage as NodeIncomingMessage } from "unenv/runtime/node/http/_request";
-import { ServerResponse as NodeServerResponse } from "unenv/runtime/node/http/_response";
+import {
+  IncomingMessage as NodeIncomingMessage,
+  ServerResponse as NodeServerResponse,
+} from "unenv/runtime/node/http";
 import type { App } from "../app";
 import type { HTTPMethod } from "../types";
 import { createError, isError, sendError } from "../error";
