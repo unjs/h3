01b7b641131a9ff569388a78eb9c0c8f7c701554
Pooya Parsa <pooya@pi0.io>
2024-06-24 17:24:44 +0200

    update cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -23,6 +23,37 @@ export function isPreflightRequest(event: H3Event): boolean {
 }
 
 /**
+ * Check if the incoming request is a CORS request.
+ */
+export function isCorsOriginAllowed(
+  origin: string | undefined,
+  options: H3CorsOptions,
+): boolean {
+  const { origin: originOption } = options;
+
+  if (
+    !origin ||
+    !originOption ||
+    originOption === "*" ||
+    originOption === "null"
+  ) {
+    return true;
+  }
+
+  if (Array.isArray(originOption)) {
+    return originOption.some((_origin) => {
+      if (_origin instanceof RegExp) {
+        return _origin.test(origin);
+      }
+
+      return origin === _origin;
+    });
+  }
+
+  return originOption(origin);
+}
+
+/**
  * Append CORS preflight headers to the response.
  */
 export function appendCorsPreflightHeaders(
--- src/utils/internal/cors.ts
+++ src/utils/internal/cors.ts
@@ -40,37 +40,6 @@ export function resolveCorsOptions(
 }
 
 /**
- * Check if the incoming request is a CORS request.
- */
-export function isCorsOriginAllowed(
-  origin: string | undefined,
-  options: H3CorsOptions,
-): boolean {
-  const { origin: originOption } = options;
-
-  if (
-    !origin ||
-    !originOption ||
-    originOption === "*" ||
-    originOption === "null"
-  ) {
-    return true;
-  }
-
-  if (Array.isArray(originOption)) {
-    return originOption.some((_origin) => {
-      if (_origin instanceof RegExp) {
-        return _origin.test(origin);
-      }
-
-      return origin === _origin;
-    });
-  }
-
-  return originOption(origin);
-}
-
-/**
  * Create the `access-control-allow-origin` header.
  */
 export function createOriginHeaders(
