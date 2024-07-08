694bdc7f29fe5541d39b6efcc4f7c95370425316
Pooya Parsa <pooya@pi0.io>
2024-07-08 10:46:53 +0200

    disable typecheck on bench

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    typecheck: { enabled: true },
+    typecheck: { enabled: !process.argv.includes("bench") },
     coverage: {
       include: ["src/**/*.ts"],
     },
