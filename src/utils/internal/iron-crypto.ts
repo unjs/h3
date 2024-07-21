5a5a13ee5f4a0cf3700d57a20cb68ead95b211a1
Pooya Parsa <pooya@pi0.io>
2024-07-21 19:15:18 +0200

    fix issue

--- src/utils/internal/iron-crypto.ts
+++ src/utils/internal/iron-crypto.ts
@@ -361,14 +361,14 @@ function stringToUint8Array(value: string): Uint8Array {
   return encoder.encode(value);
 }
 
-// Credits to Deno Stdlib
-// prettier-ignore
-const base64urlCode = /* @__PURE__ */ [
+// ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_
+const base64Code = /* @__PURE__ */ [
   65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
   84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106,
   107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
   122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 45, 95,
 ];
+
 export function base64Encode(data: ArrayBuffer | Uint8Array | string): string {
   const buff = validateBinaryLike(data);
   if (globalThis.Buffer) {
@@ -376,34 +376,34 @@ export function base64Encode(data: ArrayBuffer | Uint8Array | string): string {
   }
   // Credits: https://gist.github.com/enepomnyaschih/72c423f727d395eeaa09697058238727
   const bytes: number[] = [];
+  let i;
   const len = buff.length;
-  let i: number;
   for (i = 2; i < len; i += 3) {
     bytes.push(
-      base64urlCode[buff[i - 2]! >> 2],
-      base64urlCode[((buff[i - 2]! & 0x03) << 4) | (buff[i - 1]! >> 4)],
-      base64urlCode[((buff[i - 1]! & 0x0f) << 2) | (buff[i]! >> 6)],
-      base64urlCode[buff[i]! & 0x3f],
+      base64Code[buff[i - 2]! >> 2],
+      base64Code[((buff[i - 2]! & 0x03) << 4) | (buff[i - 1]! >> 4)],
+      base64Code[((buff[i - 1]! & 0x0f) << 2) | (buff[i]! >> 6)],
+      base64Code[buff[i]! & 0x3f],
     );
   }
   if (i === len + 1) {
     // 1 octet yet to write
     bytes.push(
-      base64urlCode[buff[i - 2]! >> 2],
-      base64urlCode[(buff[i - 2]! & 0x03) << 4],
+      base64Code[buff[i - 2]! >> 2],
+      base64Code[(buff[i - 2]! & 0x03) << 4],
     );
   }
   if (i === len) {
     // 2 octets yet to write
     bytes.push(
-      base64urlCode[buff[i - 2]! >> 2],
-      base64urlCode[((buff[i - 2]! & 0x03) << 4) | (buff[i - 1]! >> 4)],
+      base64Code[buff[i - 2]! >> 2],
+      base64Code[((buff[i - 2]! & 0x03) << 4) | (buff[i - 1]! >> 4)],
+      base64Code[(buff[i - 1]! & 0x0f) << 2],
     );
   }
   // eslint-disable-next-line unicorn/prefer-code-point
   return String.fromCharCode(...bytes);
 }
-
 export function base64Decode(b64Url: string): Uint8Array {
   if (globalThis.Buffer) {
     return new Uint8Array(globalThis.Buffer.from(b64Url, "base64url"));
