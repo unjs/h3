f34d4ba547d2842826234f4e2b62afbcf105bd78
Pooya Parsa <pooya@pi0.io>
2024-12-04 23:44:47 +0100

    update

--- examples/server-sent-events.ts
+++ examples/server-sent-events.ts
@@ -1,22 +1,19 @@
-import { createH3, createEventStream, defineEventHandler } from "h3";
+import { createH3, createEventStream } from "h3";
 
 export const app = createH3();
 
-app.get(
-  "/",
-  defineEventHandler((event) => {
-    const eventStream = createEventStream(event);
+app.get("/", (event) => {
+  const eventStream = createEventStream(event);
 
-    // Send a message every second
-    const interval = setInterval(async () => {
-      await eventStream.push("Hello world");
-    }, 1000);
+  // Send a message every second
+  const interval = setInterval(async () => {
+    await eventStream.push("Hello world");
+  }, 1000);
 
-    // cleanup the interval when the connection is terminated or the writer is closed
-    eventStream.onClosed(() => {
-      clearInterval(interval);
-    });
+  // cleanup the interval when the connection is terminated or the writer is closed
+  eventStream.onClosed(() => {
+    clearInterval(interval);
+  });
 
-    return eventStream.send();
-  }),
-);
+  return eventStream.send();
+});
