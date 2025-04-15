3757072b0c358790008fb888a44e53c0c4b42f4d
kricsleo <kricsleo@163.com>
2025-04-15 19:01:11 +0800

    fix: use `Response.bytes` to normalize `FormData`

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -92,8 +92,11 @@ export function readRawBody<E extends Encoding = "utf8">(
       }
       // TODO: Handle other BodyInit types
       // https://developer.mozilla.org/en-US/docs/Web/API/Response/Response#body
-      if (_resolved instanceof URLSearchParams || _resolved instanceof FormData) {
+      if (_resolved instanceof URLSearchParams) {
         return Buffer.from(_resolved.toString());
+      }
+      if(_resolved instanceof FormData) {
+        return new Response(_resolved).bytes();
       }
       return Buffer.from(_resolved);
     });
